f086c75f6fd85336b0aa71a8b4f76d60931061aa
Hamza-jehad <yasamer569@gmail.com>
2022-07-04 08:52:38 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let starbauks = document.querySelector('.stbauks');
+let circl = document.querySelector('.circl');
+// let textStarbucks = document.querySelector('.stt');
+
+function imgSlider(x){
+    starbauks.src = x;
+}
+function changeColorCircl(color){
+    circl.style.background = color;
+}
+// function changeColorText(col){
+//     textStarbucks.style.color = col;
+// }
+
